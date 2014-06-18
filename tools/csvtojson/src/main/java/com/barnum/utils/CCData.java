333909edb37332e972eb8dace2e855bf511af2fc
Dave Barnum <dave.barnum@effectiveui.com>
2014-06-18 16:00:38 -0400

    added files I missed from previous commit

---
+++ tools/csvtojson/src/main/java/com/barnum/utils/CCData.java
@@ -0,0 +1,43 @@
+package com.barnum.utils;
+
+public class CCData {
+	
+	private String year;
+	private String siteName;
+	private String siteNumber;
+	private String latitude;
+	private String longitude;
+	
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getSiteName() {
+		return siteName;
+	}
+	public void setSiteName(String siteName) {
+		this.siteName = siteName;
+	}
+	public String getSiteNumber() {
+		return siteNumber;
+	}
+	public void setSiteNumber(String siteNumber) {
+		this.siteNumber = siteNumber;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	
+
+}
