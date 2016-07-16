34711b8932fcb31618863a6c87a37579dd6e1549
Dave Barnum <davebarnum@DAVEBARN-MBP.local>
2016-07-15 20:35:30 -0400

    progress

---
+++ src/app/weather/weather.service.js
@@ -0,0 +1,35 @@
+// (function() {
+//   'use strict';
+//   angular
+//     .module('weather.service')
+//     .factory('WeatherService', WeatherService);
+//
+//   angular.module('weather.service').constant('WeatherServiceConstants',{
+//     KEY:'b56db60a3d9ab1ee',
+//     URL_TEMPLATE:'http://api.wunderground.com/api/<%= key %>/conditions/q/<%= latitude %>,<%= longitude %>.json'
+//   })
+//
+//
+//
+//   WeatherService.$inject = ['$http', '$log','WeatherServiceConstants'];
+//
+//   /* @ngInject */
+//   function WeatherService($http, $log,WeatherConstants) {
+//     var service = {
+//       getConditions: getConditions
+//     };
+//
+//     return service;
+//     ////////////////
+//
+//     function getConditions(latitude, longitude) {
+//       var urlTemplate = _.template(WeatherConstants.URL_TEMPLATE);
+//       var url = urlTemplate ({latitude:latitude,longitude:longitude,key:WeatherConstants.KEY});
+//       $log.debug('url:' + url);
+//       return $http.get(url);
+//     }
+//     //http://api.wunderground.com/api/<%= key %>/history_YYYYMMDD/q/<%= latitude %>,<%= longitude %>.json'
+//     //http://api.wunderground.com/api/b56db60a3d9ab1ee/history_19980814/q/43.905984,-74.946327.json'
+//     function getHistory(latitude,longitude)
+//   }
+// })();
---
+++ src/app/weather/weather.service.spec.js
@@ -0,0 +1,26 @@
+// 'use strict';
+// describe('WeatherService', function () {
+//
+//   var WeatherService, $httpBackend;
+//   beforeEach(module('weather.service'));
+//
+//   beforeEach(inject(function (_WeatherService_, $injector) {
+//     $httpBackend = $injector.get('$httpBackend');
+//     WeatherService = _WeatherService_;
+//   }));
+//
+//   describe('getConditions', function () {
+//     var testData = [
+//       {
+//         latitude:0,
+//         longitude:0
+//       }
+//     ];
+//     beforeEach(function () {
+//     });
+//
+//     it('should use latitude and longitude to get conditions', function () {
+//     });
+//   });
+// });
+//
